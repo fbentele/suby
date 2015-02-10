91f5e9e026089389ed88abd1be4142fcaeb4f289
Florian Bentele <fbentele@gmail.com>
2015-02-10 20:00:32 +0100

    a lot...

--- src/main/java/ch/flokus/suby/service/DBSource.java
+++ src/main/java/ch/flokus/suby/service/DBSource.java
@@ -7,40 +7,41 @@ import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 
 public class DBSource {
-    static ConnectionSource c = null;
+	static ConnectionSource c = null;
 
-    private DBSource() {
-    }
+	private DBSource() {
+	}
 
-    private static void connect() {
-        String home = System.getProperty("user.home");
-        home += "/.suby/";
-        File subyhome = new File(home);
-        if (!subyhome.exists()) {
-            System.out.println("creating directory: " + subyhome);
-            boolean result = false;
-            try {
-                subyhome.mkdir();
-                result = true;
-            } catch (SecurityException se) {
-            }
-            if (result) {
-                System.out.println("DIR created");
-            }
-        }
+	private static void connect() {
+		String home = System.getProperty("user.home");
+		home += "/.suby/";
+		File subyhome = new File(home);
+		if (!subyhome.exists()) {
+			System.out.println("creating directory: " + subyhome);
+			boolean result = false;
+			try {
+				subyhome.mkdir();
+				result = true;
+			} catch (SecurityException se) {
+			}
+			if (result) {
+				System.out.println("DIR created");
+			}
+		}
 
-        String databaseUrl = "jdbc:sqlite:" + home + "suby.db";
-        try {
-            c = new JdbcConnectionSource(databaseUrl);
-        } catch (SQLException e) {
+		String databaseUrl = "jdbc:sqlite:" + home + "suby.db";
+		try {
+			c = new JdbcConnectionSource(databaseUrl);
 
-        }
-    }
+		} catch (SQLException e) {
 
-    public static ConnectionSource getConnection() {
-        if (c == null) {
-            connect();
-        }
-        return c;
-    }
+		}
+	}
+
+	public static ConnectionSource getConnection() {
+		if (c == null) {
+			connect();
+		}
+		return c;
+	}
 }
--- src/main/java/ch/flokus/suby/service/SettingsService.java
+++ src/main/java/ch/flokus/suby/service/SettingsService.java
@@ -12,69 +12,68 @@ import com.j256.ormlite.table.TableUtils;
 
 public class SettingsService {
 
-    Dao<Settings, String> settingsDao = null;
+	Dao<Settings, String> settingsDao = null;
 
-    public SettingsService() {
-        try {
-            settingsDao = DaoManager.createDao(DBSource.getConnection(), Settings.class);
-            TableUtils.createTableIfNotExists(DBSource.getConnection(), Settings.class);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+	public SettingsService() {
+		try {
+			settingsDao = DaoManager.createDao(DBSource.getConnection(), Settings.class);
+			TableUtils.createTableIfNotExists(DBSource.getConnection(), Settings.class);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public String getSetting(String key) {
-        String ret ="";
-        try {
-            Settings s = settingsDao.queryForId(key);
-            if (s != null) {
-                ret = s.getValue();
-            }
-        } catch (SQLException e) {
-        }
-        return ret;
-    }
+	public String getSetting(String key) {
+		String ret = "";
+		try {
+			Settings s = settingsDao.queryForId(key);
+			if (s != null) {
+				ret = s.getValue();
+			}
+		} catch (SQLException e) {
+		}
+		return ret;
+	}
 
-    public void insertSetting(String key, String value) {
-        Settings s = new Settings();
-        s.setKey(key);
-        s.setValue(value);
-        try {
-            settingsDao.create(s);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+	public void insertSetting(String key, String value) {
+		Settings s = new Settings();
+		s.setKey(key);
+		s.setValue(value);
+		try {
+			settingsDao.create(s);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void isertOrUpdateSetting(String key, String value) {
-        // special treatment when server url is changed
-        if(key.equals("server") && !value.startsWith("http")){
-            value = "http://" + value;
-        }
-        
-        try {
-            Settings s = settingsDao.queryForId(key);
-            if (s == null) {
-                s = new Settings();
-                s.setKey(key);
-                s.setValue(value);
-                settingsDao.create(s);
-            } else {
-                s.setValue(value);
-                settingsDao.update(s);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+	public void isertOrUpdateSetting(String key, String value) {
+		// special treatment when server url is changed
+		if (key.equals("server") && !value.startsWith("http")) {
+			value = "http://" + value;
+		}
 
-    public List<Settings> getAll() {
-        try {
-            return settingsDao.queryForAll();
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return new ArrayList<Settings>();
-    }
+		try {
+			Settings s = settingsDao.queryForId(key);
+			if (s == null) {
+				s = new Settings();
+				s.setKey(key);
+				s.setValue(value);
+				settingsDao.create(s);
+			} else {
+				s.setValue(value);
+				settingsDao.update(s);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<Settings> getAll() {
+		try {
+			return settingsDao.queryForAll();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return new ArrayList<Settings>();
+	}
 }
