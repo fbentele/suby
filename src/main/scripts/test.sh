2192980c15a538f56b38d4392ae88d1d13e6e02a
Florian Bentele <fbentele@c14416.namics.home>
2015-02-10 15:59:01 +0100

    commented out after build script

--- src/main/scripts/test.sh
+++ src/main/scripts/test.sh
@@ -1,6 +1,6 @@
 echo "Remove old Suby.jar"
-rm ~/.suby/Suby_last.jar
+#rm ~/.suby/Suby_last.jar
 echo "Move current version to old"
-mv ~/.suby/Suby.jar ~/.suby/Suby_last.jar
+#mv ~/.suby/Suby.jar ~/.suby/Suby_last.jar
 echo "Copy target to new app"
-cp target/suby-0.0.1-SNAPSHOT.jar ~/.suby/Suby.jar
+#cp target/suby-0.0.1-SNAPSHOT.jar ~/.suby/Suby.jar
