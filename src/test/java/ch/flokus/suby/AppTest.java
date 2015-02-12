81840738d567e48d4ba5c3c0fc9b8784d4068cad
Florian Bentele <fbentele@gmail.com>
2015-02-12 13:48:32 +0100

    fixed testcases

--- src/test/java/ch/flokus/suby/AppTest.java
+++ src/test/java/ch/flokus/suby/AppTest.java
@@ -1,53 +1,21 @@
 package ch.flokus.suby;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.text.Normalizer;
-
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import ch.flokus.suby.service.AppUtils;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName
-     *            name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
+    public void testAppUtilsReplacer() {
         String s = "Premiers sympt&#244;mes";
-        Normalizer.normalize(s, Normalizer.Form.NFD);
-        System.out.println(s);
-        String e;
-        try {
-            e = new String(s.getBytes("UTF-8"), "UTF-8");
-            System.out.println(e);
-        } catch (UnsupportedEncodingException e1) {
-            e1.printStackTrace();
-        }
-        try {
-            System.out.println(URLEncoder.encode(s, "UTF-8"));
-            
-        } catch (UnsupportedEncodingException e1) {
-            e1.printStackTrace();
-        }
+        String exp = "Premiers symptômes";
+        String cleaned = AppUtils.replaceAsciiChars(s);
+        assertEquals(exp, cleaned);
+    }
+
+    public void testAppUtilsHex() {
+        String s = "mySuperPassword";
+        String exp = "00000000006d79537570657250617373776f7264";
+        String hex = AppUtils.toHex(s);
+        assertEquals(exp, hex);
     }
 }
--- src/test/java/ch/flokus/suby/RestTest.java
+++ src/test/java/ch/flokus/suby/RestTest.java
@@ -1,32 +1,12 @@
 package ch.flokus.suby;
 
 import junit.framework.TestCase;
-import ch.flokus.suby.model.Album;
-import ch.flokus.suby.model.Artist;
-import ch.flokus.suby.model.Song;
-import ch.flokus.suby.rest.RestAlbums;
-import ch.flokus.suby.rest.RestArtists;
-import ch.flokus.suby.rest.RestSongs;
+import ch.flokus.suby.rest.Status;
 
 public class RestTest extends TestCase {
-    public void testInterpret() {
-        RestArtists i = new RestArtists();
-        for (Artist s : i.getAll()) {
-            System.out.println(s.getName());
-        }
-    }
-
-    public void testAlbum() {
-        RestAlbums a = new RestAlbums();
-        for (Album album : a.getAllForArtist(185)) {
-            System.out.println(album.getName() + " (" + album.getYear() + ")");
-        }
-    }
-
-    public void testSongs() {
-        RestSongs s = new RestSongs();
-        for (Song song : s.getSongsForAlbum(15)) {
-            System.out.println(song.getTitle());
-        }
+    public void testConnection() {
+        Status status = Status.getInstance();
+        String res = status.testConnection();
+        assertNotNull(res);
     }
 }
