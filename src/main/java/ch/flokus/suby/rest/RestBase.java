f4f6fb4437c93d072346d5ba274ca0f54c92899c
Florian Bentele <florian.bentele@namics.com>
2015-06-18 09:06:45 +0200

    fixed merge error

--- src/main/java/ch/flokus/suby/rest/RestBase.java
+++ src/main/java/ch/flokus/suby/rest/RestBase.java
@@ -22,42 +22,43 @@ import ch.flokus.suby.service.SettingsService;
 import ch.flokus.suby.settings.SettingsConstants;
 
 public class RestBase {
-    private SettingsService setService = null;
-    private String user = "";
-    private String pass = "";
-    private String server = "";
-    private String appname = "";
-    private static RestBase instance = null;
+	private SettingsService setService = null;
+	private String user = "";
+	private String pass = "";
+	private String server = "";
+	private String appname = "";
+	private static RestBase instance = null;
 
-    public static RestBase getInstance() {
-        if (instance == null) {
-            instance = new RestBase();
-        }
-        return instance;
-    }
+	public static RestBase getInstance() {
+		if (instance == null) {
+			instance = new RestBase();
+		}
+		return instance;
+	}
 
-    private RestBase() {
-        setService = new SettingsService();
-        refresh();
-    }
+	private RestBase() {
+		setService = new SettingsService();
+		refresh();
+	}
 
-    public void refresh() {
-        user = setService.getSetting(SettingsConstants.USERNAME);
-        pass = AppUtils.toHex(setService.getSetting(SettingsConstants.PASSWORD));
-        server = setService.getSetting(SettingsConstants.SERVER);
-        appname = setService.getSetting(SettingsConstants.APPNAME);
-    }
+	public void refresh() {
+		user = setService.getSetting(SettingsConstants.USERNAME);
+		pass = AppUtils.toHex(setService.getSetting(SettingsConstants.PASSWORD));
+		server = setService.getSetting(SettingsConstants.SERVER);
+		appname = setService.getSetting(SettingsConstants.APPNAME);
+	}
 
-    public JSONObject getJson(String view) {
-        String restbase = server + "/rest/" + view + "?u=" + user + "&p=enc:" + pass + "&v=1.10.0&c=" + appname + "&f=json";
-        return request(restbase);
-    }
+	public JSONObject getJson(String view) {
+		String restbase = server + "/rest/" + view + "?u=" + user + "&p=enc:" + pass
+				+ "&v=1.10.0&c=" + appname + "&f=json";
+		return request(restbase);
+	}
 
-    public JSONObject getJson(String view, String key, String value) {
-        String restbase = server + "/rest/" + view + "?u=" + user + "&p=enc:" + pass + "&v=1.10.0&c=" + appname + "&f=json&" + key + "=" + value;
-        return request(restbase);
-    }
-
+	public JSONObject getJson(String view, String key, String value) {
+		String restbase = server + "/rest/" + view + "?u=" + user + "&p=enc:" + pass
+				+ "&v=1.10.0&c=" + appname + "&f=json&" + key + "=" + value;
+		return request(restbase);
+	}
 
 	public SongModel download(String songId, boolean async) {
 		// get song meta information
@@ -97,89 +98,59 @@ public class RestBase {
 		return null;
 	}
 
-        // url for song download
-        String restbase = server + "/rest/download.view?u=" + user + "&p=enc:" + pass + "&v=1.10.0&c=" + appname + "&f=json&id=" + songId;
-        try {
-            URL server = new URL(restbase);
-            String absolute = System.getProperty("user.home") + "/Music/Suby/" + song.getPath();
-            System.out.println("---- before -----");
-            System.out.println(absolute);
-            absolute = absolute.replaceAll("[^a-zA-ZÄäÖöÜüéèà0-9!().\\-/\\ ]", "_");
-            System.out.println("---- after -----");
-            System.out.println(absolute);
-            File mp = new File(absolute);
-            if (mp.exists()) {
-                System.out.println(songId + ": already downloaded");
-            } else {
-                System.out.println("downloading id:" + songId);
-                if (async) {
-                    new AsyncDownloader(server, mp, App.mainView);
-                } else {
-                    FileUtils.copyURLToFile(server, mp);
-                }
-            }
-            absolute = "file://" + absolute.replaceAll(" ", "%20");
-            song.setPath(absolute);
-            return song;
-        } catch (MalformedURLException e) {
-            System.out.println("URL is malformed: " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("io exception" + e.getMessage());
-        }
-        return null;
-    }
+	public void getAlbumCover(Album a) {
+		String restbase = server + "/rest/getCoverArt.view?u=" + user + "&p=enc:" + pass
+				+ "&v=1.10.0&c=" + appname + "&f=json&id=" + a.getCoverArt() + "&size=100";
+		try {
+			URL server = new URL(restbase);
+			String coverPath = System.getProperty("user.home") + "/Music/Suby/"
+					+ a.getArtist().replaceAll("/", "_") + "/" + a.getName().replaceAll("/", "_")
+					+ "/al-" + a.getId() + ".jpg";
+			File cover = new File(coverPath);
+			if (!cover.exists()) {
+				new AsyncDownloader(server, cover, App.mainView);
+			}
+		} catch (MalformedURLException e1) {
+			e1.printStackTrace();
+		}
+	}
 
-    public void getAlbumCover(Album a) {
-        String restbase = server + "/rest/getCoverArt.view?u=" + user + "&p=enc:" + pass + "&v=1.10.0&c=" + appname + "&f=json&id=" + a.getCoverArt()
-                + "&size=100";
-        try {
-            URL server = new URL(restbase);
-            String coverPath = System.getProperty("user.home") + "/Music/Suby/" + a.getArtist().replaceAll("/", "_") + "/" + a.getName().replaceAll("/", "_")
-                    + "/al-" + a.getId() + ".jpg";
-            File cover = new File(coverPath);
-            if (!cover.exists()) {
-                new AsyncDownloader(server, cover, App.mainView);
-            }
-        } catch (MalformedURLException e1) {
-            e1.printStackTrace();
-        }
-    }
-
-    private JSONObject request(String restbase) {
-        URL server;
-        BufferedReader in;
-        try {
-            server = new URL(restbase);
-            URLConnection connection = server.openConnection();
-            in = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("UTF-8")));
-            String json = "";
-            String inputLine;
-            StringBuilder jsonsb = new StringBuilder();
-            while ((inputLine = in.readLine()) != null)
-                jsonsb.append(inputLine);
-            in.close();
-            json = AppUtils.replaceAsciiChars(jsonsb.toString());
-            JSONObject obj = new JSONObject(json);
-            if (obj.getJSONObject("subsonic-response").getString("status").equals("ok")) {
-                Status.getInstance().setState(ServerStatus.OK);
-                return obj.getJSONObject("subsonic-response");
-            } else {
-                String error = obj.getJSONObject("error").getString("message");
-                System.out.println(error);
-                Status.getInstance().setState(ServerStatus.UNKNOWN);
-            }
-        } catch (MalformedURLException e1) {
-            Status.getInstance().setState(ServerStatus.ERROR);
-            System.out.println(e1.getMessage());
-        } catch (IOException e1) {
-            Status.getInstance().setState(ServerStatus.ERROR);
-            System.out.println(e1.getMessage());
-        } catch (IllegalArgumentException e) {
-            System.out.println("Can not establish connection to url " + restbase);
-        } catch (JSONException e) {
-            System.out.println("Json Exception occured: " + e.getMessage());
-            System.out.println("for url: " + restbase);
-        }
-        return new JSONObject();
-    }
+	private JSONObject request(String restbase) {
+		URL server;
+		BufferedReader in;
+		try {
+			server = new URL(restbase);
+			URLConnection connection = server.openConnection();
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream(),
+					Charset.forName("UTF-8")));
+			String json = "";
+			String inputLine;
+			StringBuilder jsonsb = new StringBuilder();
+			while ((inputLine = in.readLine()) != null)
+				jsonsb.append(inputLine);
+			in.close();
+			json = AppUtils.replaceAsciiChars(jsonsb.toString());
+			JSONObject obj = new JSONObject(json);
+			if (obj.getJSONObject("subsonic-response").getString("status").equals("ok")) {
+				Status.getInstance().setState(ServerStatus.OK);
+				return obj.getJSONObject("subsonic-response");
+			} else {
+				String error = obj.getJSONObject("error").getString("message");
+				System.out.println(error);
+				Status.getInstance().setState(ServerStatus.UNKNOWN);
+			}
+		} catch (MalformedURLException e1) {
+			Status.getInstance().setState(ServerStatus.ERROR);
+			System.out.println(e1.getMessage());
+		} catch (IOException e1) {
+			Status.getInstance().setState(ServerStatus.ERROR);
+			System.out.println(e1.getMessage());
+		} catch (IllegalArgumentException e) {
+			System.out.println("Can not establish connection to url " + restbase);
+		} catch (JSONException e) {
+			System.out.println("Json Exception occured: " + e.getMessage());
+			System.out.println("for url: " + restbase);
+		}
+		return new JSONObject();
+	}
 }
